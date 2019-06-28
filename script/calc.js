ea7d7c0ce5c62e7414d17ae660c5e8e6e4084ecd
Upshur06 <mupshur2005@gmail.com>
2019-06-28 02:44:22 -0400

    selecting html elements

--- script/calc.js
+++ script/calc.js
@@ -1,1 +1,9 @@
-console.log("working");
+// console.log("working");
+
+var billAmount = document.getElementById("formGroupExampleInput1");
+
+var numOfPeople = document.getElementById("formGroupExampleInput3");
+
+var selectBtn = document.getElementsByTagName("button");
+
+var showAnswer = document.getElementById("answer");
