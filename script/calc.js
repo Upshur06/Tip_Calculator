5831ff99140faf46be21ab37a7bda6601e7fd26f
Upshur06 <mupshur2005@gmail.com>
2019-07-22 02:52:56 -0400

    practice functions

--- script/calc.js
+++ script/calc.js
@@ -4,6 +4,43 @@ var billAmount = document.getElementById("formGroupExampleInput1");
 
 var numOfPeople = document.getElementById("formGroupExampleInput3");
 
-var selectBtn = document.getElementsByTagName("button");
+var selectBtn = document.getElementById("pickBtn");
 
 var showAnswer = document.getElementById("answer");
+
+var rateService = document.getElementById("inputGroupSelect01");
+
+function tipRate(){
+  if(rateService.value === "1"){
+    console.log("Extremely Happy");
+  } else if(rateService.value === "2"){
+    console.log("Very Happy");
+  }else if(rateService.value === "3"){
+    console.log("Somewhat Happy");
+  }else if(rateService.value === "4"){
+    console.log("Not Very Happy");
+  }else if(rateService.value === "5"){
+    console.log("Not Happy At All");
+  }else{
+    console.log("Please Make a Selection")
+  }
+};
+
+
+function amount(){
+  if(billAmount){
+    console.log(billAmount.value);
+  }
+};
+
+function headCount(){
+  if(numOfPeople){
+    console.log(numOfPeople.value);
+  }
+};
+
+selectBtn.addEventListener("click", function(){
+  headCount();
+  // tipRate();
+  // amount();
+});
