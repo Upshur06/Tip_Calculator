f65c71875656842f9573962759ac71b42594d0df
Upshur06 <mupshur2005@gmail.com>
2019-07-22 04:39:13 -0400

    working equation function

--- script/calc.js
+++ script/calc.js
@@ -11,39 +11,40 @@ var showAnswer = document.getElementById("answer");
 var rateService = document.getElementById("inputGroupSelect01");
 
 
+
 function tipRate(){
-  var threePercent = parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.3);
-  var twoPercent = parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.2);
-  var onePercent = parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.1);
-  var halfPercent = parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.05);
-  var zeroPercent = parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.0);
+  var threePercent = (parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.3))/ parseFloat(numOfPeople.value);
+  var twoPercent = (parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.2))/ parseFloat(numOfPeople.value);
+  var onePercent = (parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.1))/ parseFloat(numOfPeople.value);
+  var halfPercent = (parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.05))/ parseFloat(numOfPeople.value);
+  var zeroPercent = (parseFloat(billAmount.value) + parseFloat(billAmount.value * 0.0))/ parseFloat(numOfPeople.value);
     if(rateService.value === "1"){
-      console.log(threePercent.toFixed(2));
+      console.log(threePercent.toFixed(2) + " per person!");
     } else if(rateService.value === "2"){
-      console.log(twoPercent.toFixed(2));
+      console.log(twoPercent.toFixed(2) + " per person!");
     }else if(rateService.value === "3"){
-      console.log(onePercent.toFixed(2));
+      console.log(onePercent.toFixed(2) + " per person!");
     }else if(rateService.value === "4"){
-    console.log(halfPercent.toFixed(2));
+    console.log(halfPercent.toFixed(2) + " per person!");
     }else if(rateService.value === "5"){
-      console.log(zeroPercent.toFixed(2));
+      console.log(zeroPercent.toFixed(2) + " per person!");
     }else{
       console.log("Please Make a Selection")
     }
 };
 
 
-function amount(){
-  if(billAmount){
-    console.log(billAmount.value);
-  }
-};
+// function amount(){
+//   if(billAmount){
+//     console.log(billAmount.value);
+//   }
+// };
 
-function headCount(){
-  if(numOfPeople){
-    console.log(numOfPeople.value);
-  }
-};
+// function headCount(){
+//   if(numOfPeople){
+//     console.log(numOfPeople.value);
+//   }
+// };
 
 selectBtn.addEventListener("click", function(){
   // headCount();
